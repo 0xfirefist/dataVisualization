8957e80c8675e001d63d7a23fe161440c9d7caea
Dev Kalra <devkalra1@outlook.com>
2018-08-31 19:05:00 +0530

    added some other graphs

--- app.py
+++ app.py
@@ -9,15 +9,25 @@ app = Flask("dev")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route("/submit",methods=["POST"])
 def visualize():
-    x=list(map(float,request.form["xaxis"].split(",")))
-    y=list(map(float,request.form["yaxis"].split(",")))
-    if len(x)!=len(y):
-        return "enter correct values"
-    img=BytesIO()
-    plt.plot(x,y)
-    plt.savefig(img)
-    return render_template("submit.html",img=base64.b64encode(img.getvalue()).decode())    
+	x=list(map(float,request.form["xaxis"].split(",")))
+	y=list(map(float,request.form["yaxis"].split(",")))
+	if len(x)!=len(y):
+		return render_template("<html>enter correct values</html>")
+	img=BytesIO()
+	if request.form["graph"] == "plot" :
+		plt.figure()
+		plt.plot(x,y)
+	if request.form["graph"] == "bargraph" :
+		plt.figure()
+		plt.bar(x,y)
+	if request.form["graph"] == "scatter" :
+		plt.figure()
+		plt.scatter(x,y)
+
+
+	plt.savefig(img)
+	return render_template("submit.html",img=base64.b64encode(img.getvalue()).decode())  
